0d795c6337829c51f910cb4993a47cb272e352de
ali saemi <alisaemi0005@gmail.com>
2024-12-31 01:57:52 +0330

    update Account main page

--- lib/Account/AccountMainPage.dart
+++ lib/Account/AccountMainPage.dart
@@ -6,17 +6,26 @@ import 'package:ap_finale_project_front/main.dart';
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
-  @override
-  Widget build(BuildContext context) {
+Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFDFF2EB), // رنگ پس‌زمینه
+      backgroundColor: Color(0xFFDFF2EB), // رنگ پس‌زمینه
       appBar: AppBar(
-        backgroundColor: const Color(0xFFDFF2EB), // هم‌رنگ با پس‌زمینه
+        backgroundColor: Color(0xFFDFF2EB), // هم‌رنگ با پس‌زمینه
         elevation: 0, // حذف سایه
-        leading: const Icon(Icons.arrow_back, color: Colors.black), // آیکون لوگو
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => home()), // جایگزین صفحه مقصد
+            );
+          },
+        ),
+
+        // آیکون لوگو
         actions: [
           IconButton(
-            icon: const Icon(Icons.settings, color: Colors.black), // آیکون تنظیمات
+            icon: Icon(Icons.settings, color: Colors.black), // آیکون تنظیمات
             onPressed: () {},
           ),
         ],
@@ -27,20 +36,20 @@ class Profile extends StatelessWidget {
             CircleAvatar(
               radius: 50,
               backgroundColor: Colors.grey[400],
-              backgroundImage: const AssetImage('assets/images (8).jpg'),
+              backgroundImage: AssetImage('assets/images (8).jpg'),
             ),
-            const SizedBox(height: 10),
+            SizedBox(height: 10),
             // نام و شماره تلفن کاربر
             Text(
-              '${a.fname} ${a.lname}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+              '${a.userName}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
             ),
-            const SizedBox(height: 5),
+            SizedBox(height: 5),
             Text(
               '${a.phoneNumber}',
               style: TextStyle(fontSize: 14, color: Colors.grey[700]),
             ),
-            const SizedBox(height: 20),
+            SizedBox(height: 20),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -48,52 +57,52 @@ class Profile extends StatelessWidget {
                 ElevatedButton.icon(
                   onPressed: () {},
                   icon: ClipOval(
-                    child: SizedBox(
+                    child: Container(
                       width: 30,  // عرض دایره
                       height: 30, // ارتفاع دایره
                       child: Image.asset(
                         'assets/images (18).png',
-                        fit: BoxFit.cover,  // تصویر به صورت تمام صفحه در دایره نمایش داده می‌شود
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                  label: const Text('فعال سازی کیف پول', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  label: Text('فعال سازی کیف پول', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green[200],
-                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
                   ),
                 ),
                 ElevatedButton.icon(
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Account()),
+                      MaterialPageRoute(builder: (context) => Account()),
                     );
                   },
                   icon: ClipOval(
-                    child: SizedBox(
+                    child: Container(
                       width: 30,  // عرض دایره
                       height: 30, // ارتفاع دایره
                       child: Image.asset(
                         'assets/images (9).jpg',
-                        fit: BoxFit.cover,  // تصویر به صورت تمام صفحه در دایره نمایش داده می‌شود
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                  label: const Text('ویرایش اطلاعات کاربری', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  label: Text('ویرایش اطلاعات کاربری', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue[200],
-                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 20),
+            SizedBox(height: 20),
 
             // فرم اطلاعات کاربری
             Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(15),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(15),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
@@ -101,18 +110,18 @@ class Profile extends StatelessWidget {
                   BoxShadow(
                     color: Colors.grey.withOpacity(0.3),
                     blurRadius: 10,
-                    offset: const Offset(0, 5),
+                    offset: Offset(0, 5),
                   ),
                 ],
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text('اطلاعات کاربری', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                  const Divider(color: Colors.grey),
+                  Text('اطلاعات کاربری', textAlign: TextAlign.center, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Divider(color: Colors.grey),
                   buildInfoRow('نام و نام خانوادگی', '${a.fname} ${a.lname}'),
                   buildInfoRow('تلفن همراه', '${a.phoneNumber}'),
-                  buildInfoRow('کد ملی', '${a.nationalID}'),
+                  /*buildInfoRow('کد ملی', '${a.nationalID}'),*/
                   buildInfoRow('ایمیل', '${a.email}'),
                   buildInfoRow('رمز عبور', '*' * (a.password?.length ?? 0)),
                   buildInfoRow('اشتراک', '${a.sub}'),
@@ -123,15 +132,16 @@ class Profile extends StatelessWidget {
 
 
             ),
-            const SizedBox(height: 30,),
+            SizedBox(height: 50,),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 GestureDetector(
                   onTap: () {},
-                  child: const Row(
+                  child: Row(
                     children: [
-                      Icon(Icons.favorite, color: Colors.redAccent),  // آیکون لیست علاقه مندی ها
+                      Icon(Icons.favorite, color: Colors.redAccent),
+                      // آیکون لیست علاقه مندی ها
                       SizedBox(width: 5),  // فاصله بین آیکون و متن
                       Text(
                         'لیست علاقه مندی ها',
@@ -140,7 +150,7 @@ class Profile extends StatelessWidget {
                     ],
                   ),
                 ),
-                const SizedBox(
+                Container(
                   height: 20, // ارتفاع خط
                   width: 1, // عرض خط
 
@@ -149,13 +159,13 @@ class Profile extends StatelessWidget {
                   onTap: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => const Account()),
+                      MaterialPageRoute(builder: (context) => Account()),
                     );
                   },
                   child: Row(
                     children: [
                       Icon(Icons.shopping_cart_outlined, color: Colors.blue[700]),  // آیکون سبد خرید
-                      const SizedBox(width: 5),  // فاصله بین آیکون و متن
+                      SizedBox(width: 5),  // فاصله بین آیکون و متن
                       Text(
                         'سبد خرید',
                         style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue[700]),
@@ -167,9 +177,9 @@ class Profile extends StatelessWidget {
             ),
 
 
-            const SizedBox(height: 20),
+            SizedBox(height: 20),
 
-            // اضافه کردن لیست علاقه‌مندی‌ها
+
 
 
           ],
@@ -177,32 +187,31 @@ class Profile extends StatelessWidget {
       ),
 
       bottomNavigationBar: ConvexAppBar(
-        color: const Color(0XFF757C84),
+        color: Color(0XFF757C84),
         top: -12.0,
-        activeColor: const Color(0XFF000000),
-        backgroundColor: const Color(0XFFDFF2EB),
-        initialActiveIndex: 2,
+        activeColor: Color(0XFF000000),
+        backgroundColor: Color(0XFFDFF2EB),
         style: TabStyle.textIn,
-        items: const [
+        items: [
           TabItem(icon: Icons.category_outlined, title: 'Category',),
           TabItem(icon: Icons.home, title: 'Home'),
           TabItem(icon: Icons.people, title: 'Profile'),
         ],
         onTap: (int i) {
           if (i == 0) {
-            /*Navigator.push(
-             // context,
-            //  MaterialPageRoute(builder: (context) => CategoryPage()),
-            );*/
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => category()),
+            );
           } else if (i == 1) {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const Home()),
+              MaterialPageRoute(builder: (context) => home()),
             );
           } else if (i == 2) {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const Account()),
+              MaterialPageRoute(builder: (context) => Profile()),
             );
           }
         },
@@ -214,12 +223,14 @@ class Profile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
+
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(title, style: TextStyle(fontSize: 14, color: Colors.grey[700])),
-          Text(value, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          Text(value, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
         ],
       ),
     );
   }
 }
+
