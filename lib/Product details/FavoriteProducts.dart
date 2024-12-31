b50e8c74ad820259610c26ab2a868f99373c1422
ali saemi <alisaemi0005@gmail.com>
2024-12-31 19:19:31 +0330

    edit favotite product

--- lib/Product details/FavoriteProducts.dart
+++ lib/Product details/FavoriteProducts.dart
@@ -13,8 +13,6 @@ import 'package:untitled/FakeData.dart';
 
 import 'package:flutter/material.dart';
 
-
-
 class FavoriteProducts extends StatelessWidget {
   final User user;
 
@@ -65,30 +63,47 @@ class FavoriteProducts extends StatelessWidget {
             margin: EdgeInsets.all(10),
             child: Row(
               children: [
-                Image.asset(
-                  product.img,
-                  width:80,
-                  height: 80,
-                  fit: BoxFit.fill,
+                Expanded(
+                  flex: 1, // تعیین نسبت فضای اختصاص داده شده
+                  child: Container(
+                    padding: EdgeInsets.all(8), // حاشیه داخلی برای زیبایی
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8), // گوشه‌های گرد
+                      child: Image.asset(
+                        product.img,
+                        fit: BoxFit.cover,
+                        height: 80, // ارتفاع تصویر
+                        width: double.infinity, // تطبیق عرض با فضای موجود
+                      ),
+                    ),
+                  ),
                 ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      product.title,
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                Expanded(
+                  flex: 2, // تعیین نسبت فضای اختصاص داده شده
+                  child: Container(
+                    padding: EdgeInsets.all(8), // حاشیه داخلی
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.title,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis, // برش متن طولانی
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "قیمت: ${product.price} تومان",
+                          style: TextStyle(fontSize: 14, color: Colors.green),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 5),
-                    Text(
-                      "قیمت: ${product.price} تومان",
-                      style: TextStyle(fontSize: 14, color: Colors.green),
-                    ),
-                  ],
+                  ),
                 ),
               ],
             ),
           );
+
         },
       ),
     );
