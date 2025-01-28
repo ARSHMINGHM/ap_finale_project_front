ff4e8fac26fcd240c0528f306341f0338650e20b
ali saemi <alisaemi0005@gmail.com>
2025-01-28 04:56:17 +0330

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,25 @@ import 'package:ap_finale_project_front/Login_and_SIgn up/SignUp.dart';
 import 'package:ap_finale_project_front/Login_and_SIgn up/Login.dart';
 import 'package:ap_finale_project_front/FakeData.dart';
 import 'package:ap_finale_project_front/Product.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
 List<Product> b = fakeProducts;
 User a = User(userName: "alisa",fname: "علی",lname:"صائمی",email: "alisaemi0005@gmail.com",phoneNumber: "0932565255",password: "1234",addresses: ["tehran"],shoppingCart: [fakeProducts[0]]);
-void main() {
+void main() async{
+  //clientSocket.instance.connect();
   runApp(const MyApp());
+
+
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //clientSocket.instance.connect();
+
     return MaterialApp(
       builder: (context, child) {
         return Directionality(
@@ -26,7 +35,10 @@ class MyApp extends StatelessWidget {
         );
       },
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: login(),
+      routes: {
+        '/home': (context) => Home(),
+      },
     );
   }
 }
